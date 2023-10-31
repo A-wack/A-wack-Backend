f19ac31fad8408aeab1164c978d9682392c71897
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 12:42:14 -0700

    어드민 기상송 조회 서비스 추가

--- src/main/java/com/example/musicrequestapp/domain/music/service/DailyMusicAdmin.java
+++ src/main/java/com/example/musicrequestapp/domain/music/service/DailyMusicAdmin.java
@@ -1,2 +1,37 @@
-package com.example.musicrequestapp.domain.music.service;public class DailyMusicAdmin {
+package com.example.musicrequestapp.domain.music.service;
+
+import com.example.musicrequestapp.domain.music.controller.dto.DailyMusicAdminResponse;
+import com.example.musicrequestapp.domain.music.entity.Music;
+import com.example.musicrequestapp.domain.music.repository.MusicRepository;
+import com.example.musicrequestapp.domain.user.entity.Role;
+import com.example.musicrequestapp.domain.user.entity.User;
+import com.example.musicrequestapp.domain.user.service.facade.UserFacade;
+import com.example.musicrequestapp.global.error.exception.NoPermissionException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DailyMusicAdmin {
+    private final MusicRepository musicRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public List<DailyMusicAdminResponse> execute() {
+        User user = userFacade.getUser();
+
+        if (user.getRole() == Role.ROLE_ADMIN) {
+            throw NoPermissionException.EXCEPTION;
+        }
+
+        List<Music> musicList = musicRepository.findAll();
+
+        return musicList.stream()
+                .map(DailyMusicAdminResponse::new)
+                .toList();
+    }
+
 }
