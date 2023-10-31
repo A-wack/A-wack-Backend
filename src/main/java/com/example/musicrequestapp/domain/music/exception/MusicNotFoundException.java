2c3059a5dac5a6aaded63a5654d31225e9b97119
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 14:22:22 -0700

    기상송을 찾지 못했을 때의 예외처리 추가

--- src/main/java/com/example/musicrequestapp/domain/music/exception/MusicNotFoundException.java
+++ src/main/java/com/example/musicrequestapp/domain/music/exception/MusicNotFoundException.java
@@ -1,2 +1,13 @@
-package com.example.musicrequestapp.domain.music.exception;public class MusicNotFoundException {
+package com.example.musicrequestapp.domain.music.exception;
+
+import com.example.musicrequestapp.global.error.ErrorCode;
+import com.example.musicrequestapp.global.error.exception.CustomException;
+
+public class MusicNotFoundException extends CustomException {
+    public static final CustomException EXCEPTION =
+            new MusicNotFoundException();
+
+    private MusicNotFoundException() {
+        super(ErrorCode.MUSIC_NOT_FOUND);
+    }
 }
--- src/main/java/com/example/musicrequestapp/domain/music/service/DeleteMusic.java
+++ src/main/java/com/example/musicrequestapp/domain/music/service/DeleteMusic.java
@@ -1,2 +1,34 @@
-package com.example.musicrequestapp.domain.music.service;public class DeleteMusic {
+package com.example.musicrequestapp.domain.music.service;
+
+import com.example.musicrequestapp.domain.music.entity.Music;
+import com.example.musicrequestapp.domain.music.exception.MusicNotFoundException;
+import com.example.musicrequestapp.domain.music.repository.MusicRepository;
+import com.example.musicrequestapp.domain.user.entity.Role;
+import com.example.musicrequestapp.domain.user.entity.User;
+import com.example.musicrequestapp.domain.user.service.facade.UserFacade;
+import com.example.musicrequestapp.global.error.exception.NoPermissionException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteMusic {
+    private final MusicRepository musicRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(String url) {
+        User user = userFacade.getUser();
+
+        if (user.getRole() == Role.ROLE_ADMIN) {
+            throw NoPermissionException.EXCEPTION;
+        }
+
+        Music music = musicRepository.findByUrl(url)
+                .orElseThrow(() -> MusicNotFoundException.EXCEPTION);
+
+        musicRepository.delete(music);
+    }
+
 }
