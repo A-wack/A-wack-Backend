ad0bb1cb1e3d9be4c715db0b397e130113afb5d9
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 14:22:58 -0700

    컨트롤러 변경

--- src/main/java/com/example/musicrequestapp/domain/user/controller/AdminController.java
+++ src/main/java/com/example/musicrequestapp/domain/user/controller/AdminController.java
@@ -1,13 +1,15 @@
 package com.example.musicrequestapp.domain.user.controller;
 
+import com.example.musicrequestapp.domain.music.controller.dto.DailyMusicAdminResponse;
+import com.example.musicrequestapp.domain.music.controller.dto.SelectRequest;
+import com.example.musicrequestapp.domain.music.service.DailyMusicAdmin;
+import com.example.musicrequestapp.domain.music.service.DeleteMusic;
+import com.example.musicrequestapp.domain.music.service.SelectMusic;
 import com.example.musicrequestapp.domain.post.controller.dto.response.PostResponse;
 import com.example.musicrequestapp.domain.post.service.AllPostList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +18,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminController {
     private final AllPostList allPostList;
+    private final SelectMusic selectMusic;
+    private final DeleteMusic deleteMusic;
+    private final DailyMusicAdmin dailyMusicAdmin;
+
+    @GetMapping("/daily-music/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DailyMusicAdminResponse> setDailyMusicAdmin() {
+        return dailyMusicAdmin.execute();
+    }
 
     @GetMapping("/song-list")
     @ResponseStatus(HttpStatus.OK)
     public List<PostResponse> setAllPostList() {
         return allPostList.execute();
+    }
+
+    //어드민 권한
+    @PostMapping("/select")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void setSelectMusic(@RequestBody SelectRequest request) {
+        selectMusic.execute(request);
+    }
+
+    @DeleteMapping("/{url}")
+    @ResponseStatus(HttpStatus.OK)
+    public void setDeleteMusic(@PathVariable String url) {
+        deleteMusic.execute(url);
     }
 
 }
