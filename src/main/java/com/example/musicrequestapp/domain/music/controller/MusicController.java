dfb6fea19789a98e07fa4e32647517720d0fd323
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 12:41:36 -0700

    컨트롤러 변경

--- src/main/java/com/example/musicrequestapp/domain/music/controller/MusicController.java
+++ src/main/java/com/example/musicrequestapp/domain/music/controller/MusicController.java
@@ -1,9 +1,7 @@
 package com.example.musicrequestapp.domain.music.controller;
 
 import com.example.musicrequestapp.domain.music.controller.dto.DailyMusicResponse;
-import com.example.musicrequestapp.domain.music.controller.dto.SelectRequest;
 import com.example.musicrequestapp.domain.music.service.DailyMusic;
-import com.example.musicrequestapp.domain.music.service.SelectMusic;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MusicController {
     private final DailyMusic dailyMusic;
-    private final SelectMusic selectMusic;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<DailyMusicResponse> showDailyMusic() {
         return dailyMusic.execute();
-    }
-
-    //어드민 권한
-    @PostMapping("/admin/select")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void setSelectMusic(@RequestBody SelectRequest request) {
-        selectMusic.execute(request);
     }
 
 }
