2683fb532741798949caa80a03130f89b4108927
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 12:41:25 -0700

    어드민 response 추가

--- src/main/java/com/example/musicrequestapp/domain/music/controller/dto/DailyMusicAdminResponse.java
+++ src/main/java/com/example/musicrequestapp/domain/music/controller/dto/DailyMusicAdminResponse.java
@@ -1,2 +1,20 @@
-package com.example.musicrequestapp.domain.music.controller.dto;public class DailyMusicAdminResponse {
+package com.example.musicrequestapp.domain.music.controller.dto;
+
+import com.example.musicrequestapp.domain.music.entity.Music;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DailyMusicAdminResponse {
+    private String music;
+    private String user;
+
+    public DailyMusicAdminResponse(Music music) {
+        this.music = music.getUrl();
+        this.user = music.getUser();
+    }
+
 }
