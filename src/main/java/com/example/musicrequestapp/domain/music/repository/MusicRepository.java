b1385bc4b4a75bb0dd977676205116d5f9f746f1
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 12:41:56 -0700

    데이터 검색 추가

--- src/main/java/com/example/musicrequestapp/domain/music/repository/MusicRepository.java
+++ src/main/java/com/example/musicrequestapp/domain/music/repository/MusicRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MusicRepository extends CrudRepository<Music, Long> {
     @Override
     List<Music> findAll();
+
+    Optional<Music> findByUrl(String url);
 
 }
