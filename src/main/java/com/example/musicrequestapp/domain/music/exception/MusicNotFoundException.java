e134eb75d78a6ec38120def92b1a7d320cf30007
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 12:40:47 -0700

    add

---
+++ src/main/java/com/example/musicrequestapp/domain/music/exception/MusicNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.musicrequestapp.domain.music.exception;public class MusicNotFoundException {
+}
---
+++ src/main/java/com/example/musicrequestapp/domain/music/service/DailyMusicAdmin.java
@@ -0,0 +1,2 @@
+package com.example.musicrequestapp.domain.music.service;public class DailyMusicAdmin {
+}
