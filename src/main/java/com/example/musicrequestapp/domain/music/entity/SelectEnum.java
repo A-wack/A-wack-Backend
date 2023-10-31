45567e79f3c4c4e3e94dbc45af702f4038ab517a
101 <tmddnjs001133@dsm.hs.kr>
2023-10-31 08:30:03 -0700

    Enum 파일 띄어쓰기 삭제

--- src/main/java/com/example/musicrequestapp/domain/music/entity/SelectEnum.java
+++ src/main/java/com/example/musicrequestapp/domain/music/entity/SelectEnum.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SelectEnum {
-    SELECTED("채택 되었습니다."),
-    NOT_SELECTED("거부 되었습니다."),
+    SELECTED("채택되었습니다."),
+    NOT_SELECTED("거부되었습니다."),
     CHECKING("확인 중");
 
     private final String num;
