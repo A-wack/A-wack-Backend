be8ea842a4b460152478c5e3365528e20f636c3f
101 <tmddnjs001133@dsm.hs.kr>
2023-11-10 18:48:43 +0900

    Virtual Threads 추가

--- src/main/java/com/example/musicrequestapp/MusicRequestAppApplication.java
+++ src/main/java/com/example/musicrequestapp/MusicRequestAppApplication.java
@@ -2,7 +2,11 @@ package com.example.musicrequestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.Executors;
 
 @EnableScheduling
 @SpringBootApplication
@@ -12,4 +16,8 @@ public class MusicRequestAppApplication {
         SpringApplication.run(MusicRequestAppApplication.class, args);
     }
 
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
 }
