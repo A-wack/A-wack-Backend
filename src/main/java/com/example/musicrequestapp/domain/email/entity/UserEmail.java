94d822b415029af1a878fc1c47d44c3304c7564b
101 <tmddnjs001133@dsm.hs.kr>
2023-11-12 20:37:10 +0900

    edit

--- src/main/java/com/example/musicrequestapp/domain/email/entity/UserEmail.java
+++ src/main/java/com/example/musicrequestapp/domain/email/entity/UserEmail.java
@@ -1,10 +1,10 @@
 package com.example.musicrequestapp.domain.email.entity;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Builder
